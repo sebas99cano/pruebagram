770591878fdbb141345c079cbeac6a968a0f55e4
SebastianC <sebas99cano@gmail.com>
2021-06-29 11:54:36 -0500

    funcion de login con un boton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            user:null
+        }
+    }
+
+    componentWillUnmount() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({user});
+        })
+    }
+
+    handleAuth(){
+        let provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(result =>
+                console.log(`${result.user.email} ha iniciado sesion , ${result.user.displayName} , ${result.user.photoURL}`)
+            )
+            .catch(error => console.log(`Error ${error.code} : ${error.message}`))
+    }
+
+    renderLoginButton(){
+        if(this.state.user !== null){
+
+        }else{
+            console.log(this.state)
+            return(<button onClick={this.handleAuth}>Login con Google</button>);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>PruebaGram</h1>
+                {this.renderLoginButton()}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKbi99XB62eprh5zL9DtLM4oov4KCHWqE",
+    authDomain: "pruebagram-a988e.firebaseapp.com",
+    projectId: "pruebagram-a988e",
+    storageBucket: "pruebagram-a988e.appspot.com",
+    messagingSenderId: "765407407455",
+    appId: "1:765407407455:web:6c244af9be035585771aea",
+    measurementId: "G-87CJMJTMVE"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
